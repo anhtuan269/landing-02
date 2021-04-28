7f70172859b738df06a4a9bece5b40db0e0d9711
zeroo <anhtuan090795@gmail.com>
2021-04-29 02:53:40 +0700

    feat (textcontent): responsive desktop

--- components/Sections/Text_content_1.vue
+++ components/Sections/Text_content_1.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=" max-w-d mx-auto flex justify-between  tablet:flex-col-reverse tablet:justify-center tablet:pt-100 tablet:bg-gray-100 ">
-      <div class="mt-70 w-3/5 tablet:w-full tablet:mt-2">
+  <div class=" desktop:max-w-d desktop:mx-auto desktop:bg-white flex desktop:flex-row desktop:pt-0 desktop:justify-between  tablet:flex-col-reverse tablet:justify-center tablet:pt-100 tablet:bg-gray-100 ">
+      <div class="desktop:mt-70 desktop:w-3/5 tablet:w-full tablet:mt-2">
         <img class=" tablet:mx-auto" src="@/static/image/image.png" alt="">
       </div>
       <Slide />
